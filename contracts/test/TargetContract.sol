e8791a15c95f51118b5fedfc73131aef2f3cff2a
Moody Salem <moody.salem@gmail.com>
2020-05-17 00:20:29 -0500

    Fix compilation

--- contracts/test/TargetContract.sol
+++ contracts/test/TargetContract.sol
@@ -1,17 +1,19 @@
 pragma solidity >=0.6.0;
 
+import "../interfaces/IWETH9.sol";
+
 contract TargetContract {
     uint private acceptETH;
     uint private acceptWETH;
 
     // just takes the weth from the caller
-    function targetCall(address weth, uint amount) external {
+    function targetCall(address payable weth, uint amount) external {
         require(amount <= acceptWETH, 'ACCEPT_WETH_LIMIT');
-        IWETH(weth).transferFrom(msg.sender, amount);
+        IWETH9(weth).transferFrom(msg.sender, address(this), amount);
     }
 
     // receives the eth
     receive() payable external {
-        require(msg.value <= acceptETH, 'ACCEPT_ETH_LIMIT')
+        require(msg.value <= acceptETH, 'ACCEPT_ETH_LIMIT');
     }
 }
